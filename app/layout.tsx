89e9197fa4fb651a4bc36441c854c98868d40129
Jonathan Cruz <jonathan.cruz89@gmail.com>
2024-01-02 11:18:01 -0500

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,21 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+  },
+  keywords: ['Next.js', 'Learn', 'Dashboard', 'App Router'],
+  robots: 'follow, index',
+};
 
 export default function RootLayout({
   children,
